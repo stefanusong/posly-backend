8d39aa88e4ec126e22a338f462c6cce2d386e2b8
Stefanus Ong <stefanusoms@gmail.com>
2022-02-01 17:13:14 +0700

    reset project

--- entities/foodcategory.entity.go
+++
@@ -1,7 +0,0 @@
-package entities
-
-type FoodCategory struct {
-	ID      uint64 `gorm:"primary_key:auto_increment" json:"id"`
-	Name    string `gorm:"type:varchar(255)" json:"name"`
-	RestoId uint64 `gorm:"type:varchar(255)" json:"resto_id"`
-}
--- server.go
+++ server.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"github.com/stefanusong/posly-backend/configs"
-	"github.com/stefanusong/posly-backend/helpers"
-	"github.com/stefanusong/posly-backend/routes"
-)
-
 func main() {
-	helpers.LoadEnv()
-	e, db := routes.Init()
-	defer configs.DisconnectDB(db)
-	e.Logger.Fatal(e.Start(":8080"))
+
 }
